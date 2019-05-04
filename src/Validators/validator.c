c4aeab5ed6d15c1d2b15cebc67bedc3577deae09
Victor Augusto <victor.silva452@outlook.com>
2019-05-04 17:25:36 -0300

    modification on .gitignore

---
+++ src/Validators/validator.c
@@ -0,0 +1,5 @@
+//
+// Created by victor on 04/05/19.
+//
+
+#include "validator.h"
---
+++ src/Validators/validator.h
@@ -0,0 +1,8 @@
+//
+// Created by victor on 04/05/19.
+//
+
+#ifndef CIEMULATOR_VALIDATOR_H
+#define CIEMULATOR_VALIDATOR_H
+
+#endif //CIEMULATOR_VALIDATOR_H
