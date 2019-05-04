549f24ca69c9db543cf63c8fa442dbab816f55f6
Victor Augusto <victor.silva452@outlook.com>
2019-05-04 10:44:45 -0300

    Functions insert and show created, and collect the bits is on

--- main.c
+++ main.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ever ;; //Just for Fun
+
+typedef struct truthTable{
+    int j;
+    int k;
+    int q;
+    struct truthTable *next;
+} TruthTable;
+
+TruthTable *prim= NULL, *p, *q;
+
+void insert(int i, char option){
+    p = malloc(sizeof(TruthTable));
+
+    !p ? exit(-1) : NULL;
+    switch (option){
+        case 'j':
+            p->j = i;
+            break;
+        case 'k':
+            p->k = i;
+            break;
+        default:
+            p->q = i;
+            break;
+    }
+
+    p->next = NULL;
+
+    if(prim != NULL) q->next = p; else prim = p;
+
+    q = p;
+}
+
+void showList(){
+    int i = 0;
+    for(p = prim; p != NULL; p = p->next){
+        printf("%d|%d\n________\n", i+1, p->q);
+        i++;
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int value = 0, i = 0;
+    printf("==> Entre com a sequencia de Q\n");
+    printf("Para sair digite: 11\n\n");
+    for(ever) {
+        do{
+            printf("%d => ", i+1);
+            scanf("%d", &value);
+            if(value == 11){
+                printf("%d\n", i%2);
+                if(!(i%2)) break; else printf("Insira mais um valor\n"); //Quebrando o laço inicial
+            }
+        } while(value != 0 && value != 1);
+        i++;
+
+        if(value == 11){
+            printf("Saindo...");
+            break;
+        } //Interronpendo segundo laço
+        insert(value, 'q');
+    }
+
+    showList();
     return 0;
 }
