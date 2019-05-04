8a06c60523582e2dc0d505dd09702cdccd11ba26
Victor Augusto <victor.silva452@outlook.com>
2019-05-04 00:30:26 -0300

    The party is open!

---
+++ cmake-build-debug/CMakeFiles/CiEmulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CiEmulator.dir/main.c.o"
+  "CiEmulator.pdb"
+  "CiEmulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CiEmulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
