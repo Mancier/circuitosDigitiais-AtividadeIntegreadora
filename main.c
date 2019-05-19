f5b59d12448893e75c9c62921c52d66ac9a3e7a5
Victor Augusto <victor.silva452@outlook.com>
2019-05-19 16:33:20 -0300

    Main updated, I dont remember what

--- main.c
+++ main.c
@@ -5,63 +5,40 @@
 #define ever ;; //Just for Fun
 
 typedef struct truthTable{
-    int j;
-    int k;
+    char j;
+    char k;
     int q;
     struct truthTable *next;
 } TruthTable;
 
 TruthTable *prim= NULL, *p, *q;
 
-void insert(int i, char option){
+void insert(int i){
     p = malloc(sizeof(TruthTable));
-
     !p ? exit(-1) : NULL;
-
-    switch (option){
-        case 'j':
-            p->j = i;
-            break;
-        case 'k':
-            p->k = i;
-            break;
-        default:
-            p->q = i;
-            break;
-    }
-
+    p->q = i;
     p->next = NULL;
-
     if(prim != NULL) q->next = p; else prim = p;
-
     q = p;
-}
-
-void showList(){
-    int i = 0;
-    for(p = prim; p != NULL; p = p->next){
-        printf("%d -> %d\n", i+1, p->q);
-        i++;
-    }
 }
 
 void equalsBits(TruthTable *pointer){
     if(pointer->q == 1){
-        pointer->j = 2;
-        pointer->k = 0;
+        pointer->j = 'X';
+        pointer->k = '0';
     } else {
-        pointer->j = 0;
-        pointer->k = 2;
+        pointer->j = '0';
+        pointer->k = 'X';
     }
 }
 
 void differentBits(TruthTable *pointer){
     if(pointer->q == 1){
-        pointer->j = 2;
-        pointer->k = 1;
+        pointer->j = 'X';
+        pointer->k = '1';
     } else {
-        pointer->j = 1;
-        pointer->k = 2;
+        pointer->j = '1';
+        pointer->k = 'X';
     }
 }
 
@@ -71,14 +48,14 @@ void differentBits(TruthTable *pointer){
  * que irao determinar o j e k desses casos
  */
 void sorting(TruthTable *pointer){
-    if(pointer->next){
-        if((pointer->q + pointer->next->q) == 1){
+    if (pointer->next) {
+        if ((pointer->q + pointer->next->q) == 1) {
             differentBits(pointer);
         } else {
             equalsBits(pointer);
         }
     } else {
-        if((pointer->q + prim->q) == 1){
+        if ((pointer->q + prim->q) == 1) {
             differentBits(pointer);
         } else {
             equalsBits(pointer);
@@ -88,14 +65,15 @@ void sorting(TruthTable *pointer){
 
 void showTable(TruthTable *pointer){
     int i = 0;
-    printf("\n============ Resultado ============\n");
-    printf("I | Qn | J | K\n");
-    printf("--------------\n");
-    for(p = prim; p->next != NULL; p = p->next){
-        printf("%d| %d | %d | %d\n", i++, pointer->q, pointer->j, pointer->k);
-        printf("--------------\n");
+    printf("\n======= Resultado =======\n");
+    printf("\tI -> Q || J | K\n");
+//    printf("--------------\n");
+    for(pointer = prim; pointer->next != NULL; pointer = pointer->next){
+        printf("\t%d -> %d || %c | %c\n", i+1, pointer->q, pointer->j, pointer->k);
+//        printf("--------------\n");
         i++;
     }
+    printf("\t%d -> %d || %c | %c\n", i+1, pointer->q, pointer->j, pointer->k);
 }
 
 int main() {
@@ -111,18 +89,23 @@ int main() {
         do {
             printf("%d => ", i + 1);
             scanf("%d", &value);
-            if (value == 11) {
-                if (!(i % 2))
-                    break; else printf("Insira mais um valor e depois digite 11 para sair\n"); //Quebrando o laço inicial
-            } else {
+            if (value == 11){
+                if(i % 2) {
+                    printf("Insira mais um valor e depois digite 11 para sair\n");
+                } else {
+                    break;
+                }
+            }
+            if(value != 0 && value != 1){
+                printf("->> Valor Invalido!\nInsira um novo valor\n");
+            }
+            else {
                 i++;
-                insert(value, 'q');
+                insert(value);
             }
         } while (value != 0 && value != 1);
     }
     // ======= Fim =======
-
-    showList();
 
     /*
      * Tratamento dessas informações de modo interessante
@@ -130,6 +113,8 @@ int main() {
     for (p = prim; p->next != NULL; p = p->next) {
         sorting(p);
     }
+    sorting(p);
+
     showTable(prim);
     return 0;
 }
