ce04caabce2fce2b489dc4962a54eb5f526981e9
Victor Augusto <victor.silva452@outlook.com>
2019-07-09 19:20:55 -0300

    Some corrections...

--- src/Validators/validator.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by victor on 04/05/19.
-//
-
-#include "validator.h"
--- src/Validators/validator.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by victor on 04/05/19.
-//
-
-#ifndef CIEMULATOR_VALIDATOR_H
-#define CIEMULATOR_VALIDATOR_H
-
-#endif //CIEMULATOR_VALIDATOR_H
